1a769d9b04293a3f5ef0c61c35d68e79a0822b48
App Academy Student <appacademy@Apps-Mac-mini-101.local>
2016-01-26 12:00:13 -0500

    Basic react/flux architecture framework. Hello World

---
+++ frontend/sine.jsx
@@ -0,0 +1,21 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
