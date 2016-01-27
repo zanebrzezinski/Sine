8c7357e92662e4d8bcc4b0765dfb9a3f003ee5ae
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2016-01-27 17:42:15 -0500

    More styling.  Another commit for herokus sake.

--- frontend/components/loop.jsx
+++ frontend/components/loop.jsx
@@ -2,12 +2,18 @@ var React = require('react');
 
 var Loop = React.createClass({
 
+  getInitialState: function(){
+    return (
+      {muted: true, date_created: new Date(this.props.loop.created_at)}
+    );
+  },
 
   render: function() {
     return(
-      <video className="loop" loop controls autoPlay muted
-        src={this.props.loop.url}
-      />
+      <div className="loop-box">
+        <div className="author">{this.props.loop.author}</div>
+        <video className="loop" loop controls autoPlay muted src={this.props.loop.url}/>
+      </div>
     );
   }
 });
