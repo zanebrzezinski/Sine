04d69b8950eab573324bbc4e791e384ad3238177
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2016-01-27 18:49:34 -0500

    Add comment box that will probably get pulled out later... more styling updates.

--- frontend/components/loop.jsx
+++ frontend/components/loop.jsx
@@ -4,15 +4,24 @@ var Loop = React.createClass({
 
   getInitialState: function(){
     return (
-      {muted: true, date_created: new Date(this.props.loop.created_at)}
+      {muted: true, date_created: new Date(this.props.loop.created_at), comment: null}
     );
+  },
+
+  commentChange: function(e){
+    this.setState({comment: e.currentTarget.value});
   },
 
   render: function() {
     return(
       <div className="loop-box">
-        <div className="author">{this.props.loop.author}</div>
+        <div className="loop-info">
+          <div className="author">{this.props.loop.author}</div>
+          <div className="created_at">{this.state.date_created.toDateString()}</div>
+        </div>
         <video className="loop" loop controls autoPlay muted src={this.props.loop.url}/>
+        <div className="title">{this.props.loop.title}</div>
+        <input onChange={this.commentChange} className="comment-box" type="text" placeholder="Say Something Nice" value={this.state.comment}/>
       </div>
     );
   }
