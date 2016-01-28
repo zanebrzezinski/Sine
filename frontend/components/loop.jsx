c091fdc1d062d48ce63e76acabffdc2c1c31cde6
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2016-01-27 19:32:05 -0500

    Add click-to-mute logic on video.  Add font awesome icons to display muted.

--- frontend/components/loop.jsx
+++ frontend/components/loop.jsx
@@ -4,7 +4,7 @@ var Loop = React.createClass({
 
   getInitialState: function(){
     return (
-      {muted: true, date_created: new Date(this.props.loop.created_at), comment: null}
+      {muted: "muted", date_created: new Date(this.props.loop.created_at), comment: null}
     );
   },
 
@@ -12,14 +12,30 @@ var Loop = React.createClass({
     this.setState({comment: e.currentTarget.value});
   },
 
+  muteLogic: function(e){
+    if (this.state.muted === "muted") {
+      this.setState({muted: ""});
+    } else {
+      this.setState({muted: "muted"});
+    }
+  },
+
   render: function() {
+    var icon;
+    if (this.state.muted === "muted") {
+      icon = "fa fa-volume-off volume";
+    } else {
+      icon = "fa fa-volume-up volume";
+    }
+
     return(
       <div className="loop-box">
         <div className="loop-info">
           <div className="author">{this.props.loop.author}</div>
           <div className="created_at">{this.state.date_created.toDateString()}</div>
         </div>
-        <video className="loop" loop controls autoPlay muted src={this.props.loop.url}/>
+        <i className={icon} ></i>
+        <video onClick={this.muteLogic} className="loop" loop autoPlay muted={this.state.muted} src={this.props.loop.url}></video>
         <div className="title">{this.props.loop.title}</div>
         <input onChange={this.commentChange} className="comment-box" type="text" placeholder="Say Something Nice" value={this.state.comment}/>
       </div>
