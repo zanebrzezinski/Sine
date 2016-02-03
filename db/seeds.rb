af679714fb12d8878652f33ef9931326b121d3ac
App Academy Student <appacademy@Apps-Mac-mini-87.local>
2016-02-03 00:26:03 -0500

    Delete video seeds for herokus sake. Add guest account seed and follows.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,9 @@ degrom = User.create(username: "Degrom", password: "degrom")
 cespedes = User.create(username: "Yoenis", password: "Cespedex")
 lagares = User.create(username: "Lagares", password: "Lagares")
 duda = User.create(username: "Lucas", password: "dudaduda")
+guest = User.create(username: "Guest", password: "password")
 
 Loop.delete_all
-loop = Loop.create(title: "Test", author_id: bartolo.id, loop_video: File.new("#{Rails.root}/app/assets/videos/temp_video.mp4"))
-loop2 = Loop.create(title: "Spunky", author_id: degrom.id, loop_video: File.new("#{Rails.root}/app/assets/videos/temp_video.mp4"))
-loop3 = Loop.create(title: "First loop", author_id: cespedes.id, loop_video: File.new("#{Rails.root}/app/assets/videos/spunky_Tadpole_-_Google_Video_512kb.mp4"))
-loop4 = Loop.create(title: "First loop", author_id: lagares.id, loop_video: File.new("#{Rails.root}/app/assets/videos/superman_electric_earthquake_512kb.mp4"))
-loop5 = Loop.create(title: "First loop", author_id: duda.id, loop_video: File.new("#{Rails.root}/app/assets/videos/the_new_three_stooges_the_noisy_silent_movie_512kb.mp4"))
 
 Following.delete_all
 Following.create(follower_id: degrom.id, followee_id: bartolo.id)
@@ -32,6 +28,10 @@ Following.create(follower_id: duda.id, followee_id: degrom.id)
 Following.create(follower_id: lagares.id, followee_id: bartolo.id)
 Following.create(follower_id: lagares.id, followee_id: cespedes.id)
 Following.create(follower_id: lagares.id, followee_id: degrom.id)
+Following.create(follower_id: guest.id, followee_id: degrom.id)
+Following.create(follower_id: guest.id, followee_id: bartolo.id)
+Following.create(follower_id: guest.id, followee_id: cespedes.id)
+Following.create(follower_id: guest.id, followee_id: duda.id)
 
 Like.delete_all
 Like.create(liker_id: bartolo.id, loop_id: loop.id)
